06d32d0ecfaf22a0543f09f430e9af71d80456cb
qhuard <huard.quentin@gmail.com>
2012-12-14 22:37:43 +0100

    HelloWorldForward

--- src/main/java/fr/todooz/web/servlet/HelloWorldServlet.java
+++ src/main/java/fr/todooz/web/servlet/HelloWorldServlet.java
@@ -1,7 +1,6 @@
 package fr.todooz.web.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,21 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorldServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	  private static final long serialVersionUID = 1L;
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-    	String name = request.getParameter("name");
-
-        if (name == null) {
-           name = (String) request.getSession(true).getAttribute("name");
-        }
-
-        //response.getWriter().write("Hello " + name + " !");
-        
-        PrintWriter writer = response.getWriter();
-
-        writer.write("<html><head></head><body><b>Hello</b> " + name + " !</body></html>");
-    }
-}
+	  @Override
+	  protected void doGet(HttpServletRequest request,
+	      HttpServletResponse response) throws ServletException, IOException {
+	    request.getRequestDispatcher("index.html").forward(request, response);
+	  }
+	}
