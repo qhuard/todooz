abb34ae115ba1451b9d765eb37c35f87aa05b0c7
qhuard <huard.quentin@gmail.com>
2012-12-17 02:04:47 +0100

    Finishing TDD&TagCloud

--- src/main/java/fr/todooz/util/TagCloud.java
+++ src/main/java/fr/todooz/util/TagCloud.java
@@ -1,18 +1,53 @@
 package fr.todooz.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TagCloud {
 
 	private List<String> tags = new ArrayList<String>();
-
+	private List<String> toptags = new ArrayList<String>();
+	
     public void add(String tag) {
-        tags.add(tag);
+    		tags.add(tag);
+    }
+    
+    public void add(String... tags) {
+        	
+    	if (tags != null) {
+    		for (String tag : tags) {
+    			if (contains(tag) == false && tag != null && tag != "")	
+    				this.tags.add(tag);
+        	}
+    	}        		
     }
 
     public int size() {
         return tags.size();
     }
+    
+    public boolean contains(String string) {
+		return tags.contains(string);
+	}
+
+	public void top(int i) {
+		// TODO Auto-generated method stub
+		int n = 0;
+		if (tags.size() > i) {
+			while (n<i) {
+				n++;
+				String tag;
+				tag = tags.get(n);
+				toptags.add(tag);
+				}
+			tags = toptags;
+		}
+	}
+
+	public void shuffle() {
+		// TODO Auto-generated method stub
+		Collections.shuffle(tags);
+	}
 
 }
--- src/test/java/fr/todooz/util/TagCloudTest.java
+++ src/test/java/fr/todooz/util/TagCloudTest.java
@@ -13,11 +13,83 @@ public class TagCloudTest {
 	   }
 	   
 	   @Test
-	   public void size() {
-	       TagCloud tagCloud = new TagCloud();
+       public void size() {
+          TagCloud tagCloud = new TagCloud();
 
-	       tagCloud.add("java");
+          tagCloud.add("java", "java", "python", "", null);
 
-	       Assert.assertEquals(1, tagCloud.size());
-	   }
+          Assert.assertEquals(2, tagCloud.size());
+       }
+	       
+       @Test
+       public void addMutiple() {
+          TagCloud tagCloud = new TagCloud();
+
+          tagCloud.add("java", "ruby", "python");
+       }
+       
+       @Test
+       public void addEmpty() {
+          TagCloud tagCloud = new TagCloud();
+
+          tagCloud.add();
+       }
+       
+       @Test
+       public void addNull() {
+          TagCloud tagCloud = new TagCloud();
+
+          tagCloud.add((String[]) null);
+       }
+       
+       @Test
+       public void contains() {
+          TagCloud tagCloud = new TagCloud();
+
+          tagCloud.add("java");
+
+          Assert.assertTrue(tagCloud.contains("java"));
+       }
+       
+       @Test
+       public void top() {
+          TagCloud tagCloud = new TagCloud();
+
+          tagCloud.add("java", "ruby", "python", "c#", "groovy");
+          tagCloud.top(3);
+
+          Assert.assertEquals(3, tagCloud.size());
+       }
+       
+       @Test
+       public void topTooFew() {
+          TagCloud tagCloud = new TagCloud();
+
+          tagCloud.add("java");
+          tagCloud.top(3);
+
+          Assert.assertEquals(1, tagCloud.size());
+       }
+       
+       @Test
+       public void topNegative() {
+          TagCloud tagCloud = new TagCloud();
+
+          tagCloud.add("java");
+          tagCloud.top(-2);
+
+          Assert.assertEquals(0, tagCloud.size());
+       }
+       
+       @Test
+       public void shuffle() {
+          TagCloud tagCloud = new TagCloud();
+
+          tagCloud.add("java", "ruby", "python", "c#", "groovy");
+          tagCloud.shuffle();
+
+          Assert.assertEquals(5, tagCloud.size());
+          Assert.assertTrue(tagCloud.contains("java"));
+       }
+       
 	}
